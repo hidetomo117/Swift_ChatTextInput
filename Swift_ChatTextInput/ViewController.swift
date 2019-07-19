2bb5211a8f8c6ee9f1c8dfee53a16b32b6b9b5d8
hidetomo117 <hidetomo117@gmail.com>
2019-07-19 20:58:25 +0900

    add project

---
+++ Swift_ChatTextInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_ChatTextInput
+//
+//  Created by hidetomo on 2019/07/19.
+//  Copyright © 2019 Hidetomo Masuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
